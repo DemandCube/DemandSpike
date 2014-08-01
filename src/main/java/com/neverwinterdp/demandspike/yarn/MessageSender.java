1186134cd6ff1148fc5d607c1db64642fdd29743
Tuan Nguyen <tuan08@gmail.com>
2014-08-01 14:28:33 +0700

    clean old demandspike test code

---
+++ src/main/java/com/neverwinterdp/demandspike/yarn/MessageSender.java
@@ -0,0 +1,7 @@
+package com.neverwinterdp.demandspike.yarn;
+
+public class MessageSender implements Runnable {
+  public void run() {
+    System.out.println("MessageSender: run()") ;
+  }
+}
---
+++ src/main/java/com/neverwinterdp/demandspike/yarn/MessageSenderConfig.java
@@ -0,0 +1,6 @@
+package com.neverwinterdp.demandspike.yarn;
+
+public class MessageSenderConfig implements Runnable {
+  public void run() {
+  }
+}
