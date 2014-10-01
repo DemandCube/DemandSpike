5be72fed6ce349e79dc2e92995c5298429684e00
Peter Jerold Leslie <leslie@charlietech.com>
2014-10-01 19:06:03 +0530

    result send back to app master

--- src/test/java/com/neverwinterdp/demandspike/yarn/YarnWorkersTest.java
+++ src/test/java/com/neverwinterdp/demandspike/yarn/YarnWorkersTest.java
@@ -10,8 +10,6 @@ import com.neverwinterdp.hadoop.yarn.app.AppClientMonitor;
 
 public class YarnWorkersTest {
 
-  
-
   @Test
   public void testYarnContainers() throws Exception {
 		YarnConfiguration yarnConf = new YarnConfiguration();
@@ -29,16 +27,15 @@ public class YarnWorkersTest {
 				"com.neverwinterdp.demandspike.yarn.master.AsyncDemandSpikeAppMasterContainerManager",
 				"--app-rpc-port", "63200", "--app-num-of-worker", "2",
 				"--conf:yarn.resourcemanager.scheduler.address=0.0.0.0:8030",
-				"--conf:broker-connect=http://127.0.0.1:7080",
+				"--conf:broker-connect=http://www.facebook.com:80",
 				"--conf:max-duration=30000",
 				"--conf:message-size=1024",
-				"--conf:maxNumOfRequests=100"};
+				"--conf:maxNumOfRequests=200"};
 
 		AppClient appClient = new AppClient();
 		AppClientMonitor appMonitor = appClient.run(args,yarnConf);
 		appMonitor.monitor();
 		appMonitor.report(System.out);
-
-
+		
   }
 }
